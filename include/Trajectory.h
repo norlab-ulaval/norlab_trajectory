aba8f12f8e008f731f0c700308f1a551cede8d3f
simonpierredeschenes <simon-pierre.deschenes.1@ulaval.ca>
2023-03-10 10:03:46 -0500

    first commit

---
+++ include/Trajectory.h
@@ -0,0 +1,18 @@
+#ifndef NORLAB_TRAJECTORY_TRAJECTORY_H
+#define NORLAB_TRAJECTORY_TRAJECTORY_H
+
+#include <Eigen/Core>
+
+class Trajectory
+{
+public:
+    Trajectory(std::vector<std::pair<float, Eigen::Matrix4f>> poses);
+    Eigen::Matrix4f getPose(float queryTime);
+    Eigen::Matrix<float, 6, 1> getGeneralizedVelocity(float queryTime);
+    Eigen::Matrix<float, 6, 6> getPoseCovariance(float queryTime);
+
+private:
+
+};
+
+#endif
---
+++ src/Trajectory.cpp
@@ -0,0 +1,21 @@
+#include "Trajectory.h"
+
+Trajectory::Trajectory(std::vector<std::pair<float, Eigen::Matrix4f>> poses)
+{
+
+}
+
+Eigen::Matrix4f Trajectory::getPose(float queryTime)
+{
+
+}
+
+Eigen::Matrix<float, 6, 1> Trajectory::getGeneralizedVelocity(float queryTime)
+{
+
+}
+
+Eigen::Matrix<float, 6, 6> Trajectory::getPoseCovariance(float queryTime)
+{
+
+}

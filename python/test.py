321dab37715f01d34571bc85bced82282a933c07
simonpierredeschenes <simon-pierre.deschenes.1@ulaval.ca>
2023-04-27 16:19:00 -0400

    Python bindings seem to work.

--- python/test.py
+++ python/test.py
@@ -1,33 +1,12 @@
 import trajectory
 import numpy as np
+import copy
 
-pose =np.array([[0,0,0,1],
-                [0,0,0,0],
-                [0,0,0,0],
-                [0,0,0,0]])
+pose = np.eye(4)
+poses = []
+for i in range(10):
+    pose[0, 3] = float(i)
+    poses.append((float(i), copy.deepcopy(pose)))
+traj = trajectory.Trajectory(poses)
 
-pose_list=([(0,np.array([[0,0,0,1],
-                          [0,0,0,0],
-                          [0,0,0,0],
-                          [0,0,0,0]])),
-            (1,np.array([[0,0,0,1],
-                         [0,0,0,0],
-                         [0,0,0,0],
-                         [0,0,0,1]])),
-            (2,np.array([[0,0,0,1],
-                         [0,0,0,0],
-                         [0,0,0,0],
-                         [0,0,0,2]]))])
-test = []
-time = 0
-for i in range(10) :
-    time +=1
-    pose[3][3]+= 1
-    test.append((time,pose))
-
-traj_test = trajectory.Trajectory(pose_list)
-
-# print("hello")
-
-# trajectory.Trajectory(poses: List[Tuple[float, numpy.ndarray[float32[4, 4]]]])
-
+print("Hello World!")

9d6b5e934bb6f2359b766783bf233e8e403eee99
simonpierredeschenes <simon-pierre.deschenes.1@ulaval.ca>
2023-04-27 15:05:06 -0400

    First functional version.

--- src/Trajectory.cpp
+++ src/Trajectory.cpp
@@ -1,36 +1,43 @@
 #include "Trajectory.h"
 
-Trajectory::Trajectory(std::vector<std::pair<float, Eigen::Matrix4f>> poses) {
-    for (int i = 0; i < poses.size(); ++i) {
+Trajectory::Trajectory(std::vector<std::pair<float, Eigen::Matrix4f>> poses)
+{
+    for(int i = 0; i < poses.size(); ++i)
+    {
         Eigen::Matrix4d T = poses[i].second.cast<double>();
-        std::shared_ptr<steam::se3::SE3StateVar> T_vi = std::make_shared<steam::se3::SE3StateVar>(
-                lgmath::se3::Transformation(T));
-        T_vi->locked() = true;
-        std::shared_ptr<steam::vspace::VSpaceStateVar<6>> w_iv_inv = std::make_shared<steam::vspace::VSpaceStateVar<6>>(
-                Eigen::Matrix<double, 6, 1>::Zero());
-        traj.add(poses[i].first, T_vi, w_iv_inv);
+        std::shared_ptr<steam::se3::SE3StateVar> T_vi = steam::se3::SE3StateVar::MakeShared(lgmath::se3::Transformation(T));
+        std::shared_ptr<steam::vspace::VSpaceStateVar<6>> w_iv_inv = steam::vspace::VSpaceStateVar<6>::MakeShared(Eigen::Matrix<double, 6, 1>::Zero());
+        if(i == 0)
+        {
+            T_vi->locked() = true;
+            w_iv_inv->locked() = true;
+        }
+
+        traj.add(steam::traj::Time(poses[i].first), T_vi, w_iv_inv);
         problem.addStateVariable(T_vi);
         problem.addStateVariable(w_iv_inv);
-        Eigen::Vector3d reference = Eigen::Vector3d::Zero();
-//        std::shared_ptr<steam::p2p::P2PErrorEvaluator> error_func = std::make_shared<steam::p2p::P2PErrorEvaluator>(
-//                T_vi, reference.transpose(), poses[i].second.topRightCorner<3, 1>().cast<double>());
-//        std::shared_ptr<steam::StaticNoiseModel<3>> noiseModel = std::make_shared<steam::StaticNoiseModel<3>>(
-//                Eigen::Matrix3d::Identity(), steam::NoiseType::INFORMATION);
-//        std::shared_ptr<steam::L2LossFunc> lossFunc = std::make_shared<steam::L2LossFunc>();
-//        std::shared_ptr<steam::WeightedLeastSqCostTerm<3>> costTerm = std::make_shared<steam::WeightedLeastSqCostTerm<3>>(
-//                error_func, noiseModel, lossFunc);
-//        problem.addCostTerm(costTerm);
+
+        std::shared_ptr<steam::p2p::P2PErrorEvaluator> errorFunc = steam::p2p::P2PErrorEvaluator::MakeShared(T_vi, Eigen::Vector3d::Zero(), poses[i].second.topRightCorner<3, 1>().cast<double>());
+        std::shared_ptr<steam::StaticNoiseModel<3>> noiseModel = steam::StaticNoiseModel<3>::MakeShared(Eigen::Matrix3d::Identity(), steam::NoiseType::INFORMATION);
+        std::shared_ptr<steam::L2LossFunc> lossFunc = steam::L2LossFunc::MakeShared();
+        std::shared_ptr<steam::WeightedLeastSqCostTerm<3>> costTerm = steam::WeightedLeastSqCostTerm<3>::MakeShared(errorFunc, noiseModel, lossFunc);
+        problem.addCostTerm(costTerm);
     }
-//    steam::GaussNewtonSolver::Params params;
-//    steam::GaussNewtonSolver solver(problem,params);
-//    solver.optimize();
+    traj.addPriorCostTerms(problem);
+
+    steam::GaussNewtonSolver::Params params;
+    params.verbose = true;
+    steam::GaussNewtonSolver solver(problem, params);
+    solver.optimize();
 }
 
-Eigen::Matrix4f Trajectory::getPose(float queryTime) {
+Eigen::Matrix4f Trajectory::getPose(float queryTime)
+{
     return traj.getPoseInterpolator(queryTime)->value().matrix().cast<float>();
 }
 
-Eigen::Matrix<float, 6, 6> Trajectory::getPoseCovariance(float queryTime) {
+Eigen::Matrix<float, 6, 6> Trajectory::getPoseCovariance(float queryTime)
+{
     steam::Covariance covariance(problem);
     Eigen::Matrix4d pose = getPose(queryTime).cast<double>();
     std::shared_ptr<steam::se3::SE3StateVar> pose_state_var = std::make_shared<steam::se3::SE3StateVar>(lgmath::se3::Transformation(pose));
--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,13 @@
 
 int main(int argc, char **argv) {
     Eigen::Matrix4f pose;
-    pose << 1, 0, 0, 1,
+    pose << 1, 0, 0, 0,
             0, 1, 0, 0,
             0, 0, 1, 0,
             0, 0, 0, 1;
     std::vector<std::pair<float, Eigen::Matrix4f>> poses;
     for (int i = 0; i < 10; i ++){
+        pose(0, 3) = float(i);
         poses.emplace_back(i,pose);
     }
     Trajectory traj(poses);
